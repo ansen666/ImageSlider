dcceb4d5320528eace4ba636b28ccfe3df6d2f30
anhui <privateworkemail@163.com>
2017-09-28 23:55:47 +0800

    第一次提交

---
+++ app/src/main/java/com/ansen/imageslider/MainActivity.java
@@ -0,0 +1,73 @@
+package com.ansen.imageslider;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.Indicators.PagerIndicator;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
+
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+    private SliderLayout mDemoSlider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDemoSlider = (SliderLayout)findViewById(R.id.slider);
+
+        HashMap<String,String> urlMaps = new HashMap<>();
+        urlMaps.put("Hannibal", "http://static2.hypable.com/wp-content/uploads/2013/12/hannibal-season-2-release-date.jpg");
+        urlMaps.put("Big Bang Theory", "http://tvfiles.alphacoders.com/100/hdclearart-10.png");
+        urlMaps.put("House of Cards", "http://cdn3.nflximg.net/images/3093/2043093.jpg");
+
+        for(String name : urlMaps.keySet()){
+            TextSliderView textSliderView = new TextSliderView(this);
+            textSliderView
+                    .description(name)//描述
+                    .image(urlMaps.get(name))//image方法可以传入图片url、资源id、File
+                    .setScaleType(BaseSliderView.ScaleType.Fit)//图片缩放类型
+                    .setOnSliderClickListener(onSliderClickListener);//图片点击
+            textSliderView.bundle(new Bundle());
+            textSliderView.getBundle().putString("extra",name);//传入参数
+            mDemoSlider.addSlider(textSliderView);//添加一个滑动页面
+        }
+
+        mDemoSlider.setPresetTransformer(SliderLayout.Transformer.Accordion);//滑动动画
+//        mDemoSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);//默认指示器样式
+        mDemoSlider.setCustomIndicator((PagerIndicator)findViewById(R.id.custom_indicator2));//自定义指示器
+        mDemoSlider.setCustomAnimation(new DescriptionAnimation());//设置图片描述显示动画
+        mDemoSlider.setDuration(4000);//设置滚动时间，也是计时器时间
+        mDemoSlider.addOnPageChangeListener(onPageChangeListener);
+    }
+
+    private BaseSliderView.OnSliderClickListener onSliderClickListener=new BaseSliderView.OnSliderClickListener() {
+        @Override
+        public void onSliderClick(BaseSliderView slider) {
+            Toast.makeText(MainActivity.this,slider.getBundle().get("extra") + "",
+                    Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    //页面改变监听
+    private ViewPagerEx.OnPageChangeListener onPageChangeListener=new ViewPagerEx.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+
+        @Override
+        public void onPageSelected(int position) {
+            Log.d("ansen", "Page Changed: " + position);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {}
+    };
+}
